ca2a10e75058d8f7a7ac0dbb7bfd170a9fe3dbe2
ozelot <ozelot@zoho.com>
2017-06-04 15:11:03 -0500

    board array and piece objects

--- scripts/chessengine.js
+++ scripts/chessengine.js
@@ -1,1 +1,68 @@
+//creating piece objects
+
+var whitePawn = {
+  type: 'pawn',
+  color: 'white'
+};
+
+var whiteKnight = {
+  type: 'knight',
+  color: 'white'
+};
+
+var whiteBishop = {
+  type: 'bishop',
+  color: 'white'
+};
+
+var whiteRook = {
+  type: 'rook',
+  color: 'white'
+};
+
+var whiteQueen = {
+  type: 'queen',
+  color: 'white'
+};
+
+var whiteKing = {
+  type: 'king',
+  color: 'white'
+};
+
+var blackPawn = {
+  type: 'pawn',
+  color: 'black'
+};
+
+var blackKnight = {
+  type: 'knight',
+  color: 'black'
+};
+
+var blackBishop = {
+  type: 'bishop',
+  color: 'black'
+};
+
+var blackRook = {
+  type: 'rook',
+  color: 'black'
+};
+
+var blackQueen = {
+  type: 'queen',
+  color: 'black'
+};
+
+var blackKing = {
+  type: 'king',
+  color: 'black'
+};
+
 //building an array for the chess board
+
+var board = [
+  file: ["a", "b", "c", "d", "e", "f", "g", "h"],
+  rank: [1, 2, 3, 4, 5, 6, 7, 8]
+];
